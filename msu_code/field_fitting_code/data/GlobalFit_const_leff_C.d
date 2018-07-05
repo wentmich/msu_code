03c611dfc8aff0fc99e650e5bfab8c15c3bf94b0
wentmich <wentmich@umich.edu>
2018-07-05 16:44:38 -0400

    making sure this still works

--- msu_code/field_fitting_code/data/GlobalFit_const_leff_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./GlobalFit_const_leff_C.so: /mnt/misc/sw/x86_64/Debian/8/root/gnu/5.34.32/include/cintdictversion.h /mnt/misc/sw/x86_64/Debian/8/root/gnu/5.34.32/include/RVersion.h
-GlobalFit_const_leff_C__ROOTBUILDVERSION= 5.34/32
