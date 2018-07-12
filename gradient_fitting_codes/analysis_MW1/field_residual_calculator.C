9d03b818451436ec5c22bb89ee2a11a4c57a392f
wentmich <wentmich@umich.edu>
2018-07-12 16:04:57 -0400

    Updating 'field_residual_calculator.C' to include multiple error analysis functions. Also removing *~ and *.d files.

--- gradient_fitting_codes/analysis_MW1/field_residual_calculator.C
+++ gradient_fitting_codes/analysis_MW1/field_residual_calculator.C
@@ -14,6 +14,7 @@
 # include <TF1.h>
 # include <TGraph.h>
 # include <fstream>
+# include <TGraph2D.h>
 
 /* DECLARATION OF GLOBAL VARIBALES:
     sep     = delimiters used by "strtok" funciton
@@ -63,6 +64,11 @@ const int NC = 12;
 int NP = NC + 1;
 int index;
 int OrdCOSY = 6;
+double r_probes = 0.1873;
+int i, j, k, c, ee, n, m, index;
+ 
+double * fcoef = (double*) malloc( NC*NEE*NNC * sizeof(double) );
+double * Bn    = (double*) malloc( Nn * sizeof(double) );
 
 /* DECLARATION OF DATA STRUCTURES:
 	FldData = structure used to store arrays of field data   */
@@ -149,10 +155,6 @@ void read_M5_params_NORM( char *fname)
 /* reads in the Enge coefficients and field parameters from the file denoted by
 "fname." It then returns two arrays, one for the field parameters (Bn_) and one
 for the Enge coefficients (fcoef). */
-	double * Bn_;
-	double * fcoef;
-	fcoef = (double*) malloc( NC*NEE*NNC * sizeof(double) );
-	Bn = (double*) malloc( Nn * sizeof(double) );
 	FILE *fp; char line[512]; char *pch;
 	const char sep[2]=" ,";
 	fp = fopen(fname, "r");
@@ -164,11 +166,11 @@ for the Enge coefficients (fcoef). */
 	sscanf(line," %lf\n", &LEFF);
 	sprintf(line,"#"); while(strncmp(line,"#",1)==0) fgets(line,512,fp);
 	int i=2;
-	pch = strtok(line, sep); sscanf(pch,"%lf", &Bn_[i]);
+	pch = strtok(line, sep); sscanf(pch,"%lf", &Bn[i]);
 	for( i=3; i<7 ; i++ )
 	{
 		pch = strtok(NULL, sep);
-		sscanf(pch,"%lf", &Bn_[i]);
+		sscanf(pch,"%lf", &Bn[i]);
 	} 
 	sprintf(line,"#");
 	while(strncmp(line,"#",1)==0)
@@ -179,7 +181,7 @@ for the Enge coefficients (fcoef). */
 		sscanf(pch,"%i", &n);
 		pch = strtok(NULL, sep);
 		sscanf(pch,"%i", &ee);
-		for( c=0; c<NC; c++)
+		for(int c=0; c<NC; c++)
 		{
 			index = c + NC*( NEE*n + ee );
 			pch = strtok(NULL, sep);
@@ -187,7 +189,7 @@ for the Enge coefficients (fcoef). */
 		}
 		fgets(line,512,fp);
 	}
-	pch = strtok(line, sep);
+	/*pch = strtok(line, sep);
 	sscanf(pch,"%i", &n);
 	pch = strtok(NULL, sep);
 	sscanf(pch,"%i", &ee);
@@ -196,11 +198,11 @@ for the Enge coefficients (fcoef). */
 		index = c + NC*( NEE*n + ee );
 		pch = strtok(NULL, sep);
 		sscanf(pch,"%lf", &fcoef[index]);
-	}
+	}*/
 	return;
 }
 
-void Field_Simulation_via_COSY_M5_FldData(FldData *data, double * fcoef, double * Bn)
+FldData * Field_Simulation_via_COSY_M5_FldData(double * fcoef, double * Bn)
 {
 /* takes the r, t, z elements in "data" and uses them to create x, y, z points
 for the simulation. It creates a file for Enge coefficients and field paramters
@@ -208,6 +210,22 @@ to be read in by the COSY code. The field simulation is done, and the final
 field result is read into "data." */
 	char FS_fname[50]="TMP_COSY_out.txt" ;
 	double xx, yy, zz, Bxx, Byy, Bzz, Brr, Btt, tt;
+	data0=  (FldData*) malloc( Nt*Nz*sizeof(FldData) );
+	double zz, rr=r_probes, tt;
+	for( k=0; k<Nz; k++ )
+	{
+		zz = k*z_step + z_min ; //printf(" k=%i  z= %lf\n",k,z);
+		for( j=0; j<Nt; j++ )
+		{
+			index = j + Nt*k ;
+			tt = (double)j * th_step ;
+			data0[index].z = zz;
+			data0[index].t = tt;
+			data0[index].r = rr ;
+			data0[index].xc = 0.;
+			data0[index].yc = 0.;
+		}
+	}
 	bool call_COSY=1;
 	if( call_COSY )
 	{
@@ -234,7 +252,7 @@ field result is read into "data." */
 			fprintf(fpC," %lf \n", Bn[i]);
 		fprintf(fpC,"Skew field components\n");
 		for( i=0; i<Nn ; i++ )
-			fprintf(fpC," %lf \n", An[i]);
+			fprintf(fpC," %lf \n", 0);
 		fprintf(fpC,"Points for field evaluation\n");
 		fprintf(fpC,"%s\n",FS_fname);
 		for( k=0; k<Nz; k++ )
@@ -242,16 +260,16 @@ field result is read into "data." */
 			for( j=0; j<Nt; j++ )
 			{
 				index = j + Nt*k ;
-				xx = -data[index].xc + data[index].r * cos( data[index].t );
-				yy = -data[index].yc + data[index].r * sin( data[index].t );
-				fprintf(fpC, " %lf,%lf,%lf\n", xx, yy, data[index].z);
+				xx = -data0[index].xc + data0[index].r * cos( data0[index].t );
+				yy = -data0[index].yc + data0[index].r * sin( data0[index].t );
+				fprintf(fpC, " %lf,%lf,%lf\n", xx, yy, data0[index].z);
 			}
 		}
 		fprintf(fpC,"END\n");
 		fclose(fpC);
-		time0 = time( NULL );
+		float time0 = time( NULL );
 		system("runcosy91.sh COSY_field_Gen.fox");
-		time1 = time( NULL );
+		float time1 = time( NULL );
 		printf( " Nz*Nt= %i  evaluated in (sec.): %i \n", Nz*Nt, (time1-time0));
 		printf( " points/sec = %lf \n", (double)(Nz*Nt)/(double)(time1-time0) );
 		printf( " sec/point = %lf \n", (double)(time1-time0)/(double)(Nz*Nt) );
@@ -277,33 +295,119 @@ field result is read into "data." */
 				printf(" Error reading point: k%i j%i \n",k,j);
 				exit(0);
 			}
-			data[index].Br = Bxx*cos(data[index].t) + Byy*sin(data[index].t);
-			data[index].Bt =-Bxx*sin(data[index].t) + Byy*cos(data[index].t);
-			data[index].Bz = Bzz;
+			data0[index].Br = Bxx*cos(data0[index].t) + Byy*sin(data0[index].t);
+			data0[index].Bt =-Bxx*sin(data0[index].t) + Byy*cos(data0[index].t);
+			data0[index].Bz = Bzz;
 		}
 	}
 	fclose( fpCO );
-	return;
-}
+	return data0;
+} 
 
 double get_chi_squared(FldData * exp_data, FldData * model_data)
 {
 /* finds the sum of the squared residuals between two data sets and returns that
 value as "chi_squared." */
-	double add_on = 0;
-	double exp_b = 0;
-	double model_b = 0;
-	double chi_squared = 0;
+	double add_on = 0.;
+	double exp_b = 0.;
+	double model_b = 0.;
+	double chi_squared = 0.;
 	for (int i = 0; i < (Nt*Nz); ++i)
 	{
 		exp_b = exp_data[i].Br;
 		model_b = model_data[i].Br;
-		add_on = pow(2,(exp_b - model_b));
-		ch_squared = chi_squared + add_on;
+		add_on = pow((exp_b - model_b),2);
+		chi_squared = chi_squared + add_on;
 	}
 	return chi_squared;
 }
 
-double get_chi_squared_from_function(FldData * exp_data, double * fcoef, double * Bn)
+double get_average_percent_error(FldData * exp_data, FldData * model_data)
 {
+	double percent_error = 0.;
+	double exp_b = 0.;
+	double model_b = 0.;
+	double total_percent_error = 0.;
+	int points = 0;
+	for (int i = 0; i < (Nt*Nz); ++i)
+	{
+		exp_b = exp_data[i].Br;
+		model_b = model_data[i].Br;
+		if (fabs(model_b) >= 0.01)
+		{
+			if (exp_b == model_b)
+				percent_error = 0;
+			else 
+				percent_error = fabs(((exp_b - model_b)/model_b)*100);
+			total_percent_error = total_percent_error + percent_error;
+			points += 1;
+		}
+		else 
+		{
+			total_percent_error = total_percent_error;
+			points += 0;
+		}
+	}
+	double avg_percent_error = total_percent_error/(Nt*Nz);
+	return avg_percent_error;
+}
+
+double get_average_difference(FldData * exp_data, FldData * model_data)
+{
+	double difference = 0.;
+	double exp_b = 0.;
+	double model_b = 0.;
+	double total_difference = 0.;
+	for (int i = 0; i < (Nt*Nz); ++i)
+	{
+		exp_b = exp_data[i].Br;
+		model_b = model_data[i].Br;
+		difference = fabs(exp_b - model_b);
+		total_difference = total_difference +difference;
+	}
+	double avg_difference = total_difference/(Nt*Nz);
+	return avg_difference;
+}
+
+TGraph2D * graph_fld_data(FldData * data)
+{
+	double * x = (double*) malloc(sizeof(double)*Nt*Nz*0.5);
+	double * y = (double*) malloc(sizeof(double)*Nt*Nz*0.5);
+	double * z = (double*) malloc(sizeof(double)*Nt*Nz*0.5);
+	for (int i = 0; i < (Nt*Nz*0.5); ++i)
+	{
+		x[i] = data[i*2].z;
+		y[i] = data[i*2].t;
+		z[i] = data[i*2].Br;
+	}
+	TGraph2D * graph = new TGraph2D((Nt*Nz*0.5), x, y, z);
+	return graph;
+}
+
+int field_residual_calculator()
+{
+	double * fcoef_model, * fcoef_fit;
+	double * Bn_model, * Bn_fit;
+	FldData * data0_model, * data0_fit;
+	double chi_squared = 0;
+	char * fname_model = (char*) "M5_params_FSQ5_9.4Tpm_TEST.txt";
+	char * fname_fit   = (char*) "M5_sim_fit.txt";
 	
+	read_M5_params_NORM(fname_model);
+	fcoef_model = fcoef;
+	Bn_model    = Bn;
+	data0_model = Field_Simulation_via_COSY_M5_FldData(fcoef_model, Bn_model);
+
+	read_M5_params_NORM(fname_fit);
+	fcoef_fit = fcoef;
+	Bn_fit    = Bn;
+	data0_fit = Field_Simulation_via_COSY_M5_FldData(fcoef_fit, Bn_fit);
+	
+	chi_squared = get_chi_squared(data0_model, data0_fit);
+	double avg_percent_error = get_average_percent_error(data0_model, data0_fit);
+	double avg_difference    = get_average_difference(data0_model, data0_fit);
+	printf("#\chi^2 = %lg\r\n", chi_squared);
+	printf("#<Percent Error> = %lg\r\n", avg_percent_error);
+	printf("#<Difference> = %lg\r\n", avg_difference);
+	return 0;
+}
